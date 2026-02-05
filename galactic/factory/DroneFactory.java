f90c7e587cfef3f7ef0141c0b41faeded9a60bf3
NurdauletZhussip-tech <nurdaulet.zhussip@narxoz.kz>
2026-02-05 22:40:35 +0500

    Update DroneFactory.java

--- galactic/factory/DroneFactory.java
+++ galactic/factory/DroneFactory.java
@@ -1,10 +1,13 @@
 package edu.narxoz.galactic.factory;
+
 import edu.narxoz.galactic.drones.*;
 
 public class DroneFactory {
-    public static Drone getAutomaticDrone(double weight) {
-        if (weight <= 50){return new LightDrone("Auto-Light-" + (int)(Math.random() * 100), 50.0);
-        }else{
-            return new HeavyDrone("Auto-Heavy-" + (int)(Math.random() * 100), 500.0);
+    public static Drone createAutomaticDrone(double weight) {
+        if (weight <= 50.0) {
+            return new LightDrone("L-AUTO-" + (int)(Math.random() * 99), 50.0);
+        } else {
+            return new HeavyDrone("H-AUTO-" + (int)(Math.random() * 99), 500.0);
         }
-    }}
+    }
+}
